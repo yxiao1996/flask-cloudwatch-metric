05d1cde6971ac0f6252897ea0bd81a809849c38b
yxiao1996 <xiaoyu960522@sina.com>
2021-08-21 07:21:26 -0700

    Release 0.0.3 pre-alpha version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
 
 setup(
     name='flask-cloudwatch-metric',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/yxiao1996/flask-cloudwatch-metric',
     license='MIT',
     author='Yu Xiao',
